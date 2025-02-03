3e28b16ca75215f079b0b6e75d0384bbe6600b5a
noslowdwn <noslowdwn@gmail.com>
2025-02-03 17:28:55 +0100

    Added Triplets

---
+++ src/main/java/com/github/groundbreakingmc/mylib/collections/cases/ImmutableTriplet.java
@@ -0,0 +1,28 @@
+package com.github.groundbreakingmc.mylib.collections.cases;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor @Getter
+public class ImmutableTriplet<L, M, R> {
+    private final L left;
+    private final M middle;
+    private final R right;
+
+    public boolean equals(final Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        final ImmutableTriplet<?, ?, ?> other = (ImmutableTriplet<?, ?, ?>) object;
+        return this.left.equals(other.left)
+                && this.middle.equals(other.middle)
+                && this.right.equals(other.right);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31 * 17 + (this.left == null ? 0 : this.left.hashCode());
+        result = 31 * result + (this.middle == null ? 0 : this.middle.hashCode());
+        result = 31 * result + (this.right == null ? 0 : this.right.hashCode());
+        return result;
+    }
+}
---
+++ src/main/java/com/github/groundbreakingmc/mylib/collections/cases/Triplet.java
@@ -0,0 +1,29 @@
+package com.github.groundbreakingmc.mylib.collections.cases;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor @Getter @Setter
+public class Triplet<L, M, R> {
+    private L left;
+    private M middle;
+    private R right;
+
+    public boolean equals(final Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        final Triplet<?, ?, ?> other = (Triplet<?, ?, ?>) object;
+        return this.left.equals(other.left)
+                && this.middle.equals(other.middle)
+                && this.right.equals(other.right);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31 * 17 + (this.left == null ? 0 : this.left.hashCode());
+        result = 31 * result + (this.middle == null ? 0 : this.middle.hashCode());
+        result = 31 * result + (this.right == null ? 0 : this.right.hashCode());
+        return result;
+    }
+}
