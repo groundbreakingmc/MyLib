427724bb5581641f7ff7d071cd6d4283243622dd
noslowdwn <noslowdwn@gmail.com>
2025-02-08 17:10:35 +0100

    Added the ability for Vault Utils to get Permission provider

--- src/main/java/com/github/groundbreakingmc/mylib/utils/vault/VaultUtils.java
+++ src/main/java/com/github/groundbreakingmc/mylib/utils/vault/VaultUtils.java
@@ -4,6 +4,8 @@ import com.github.groundbreakingmc.mylib.utils.bukkit.BukkitProviderUtils;
 import lombok.experimental.UtilityClass;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicesManager;
 
 @UtilityClass
@@ -11,6 +13,11 @@ public final class VaultUtils {
 
     private static Chat chat;
     private static Economy economy;
+    private static Permission permission;
+
+    public static Chat getChatProvider() {
+        return getChatProvider(Bukkit.getServicesManager());
+    }
 
     public static Chat getChatProvider(final ServicesManager servicesManager) {
         if (chat != null) {
@@ -20,11 +27,27 @@ public final class VaultUtils {
         return (chat = BukkitProviderUtils.getProvider(servicesManager, Chat.class));
     }
 
+    public static Economy getEconomyProvider() {
+        return getEconomyProvider(Bukkit.getServicesManager());
+    }
+
     public static Economy getEconomyProvider(final ServicesManager servicesManager) {
         if (economy != null) {
             return economy;
         }
 
         return (economy = BukkitProviderUtils.getProvider(servicesManager, Economy.class));
+    }
+
+    public static Permission getPermissionProvider() {
+        return getPermissionProvider(Bukkit.getServicesManager());
+    }
+
+    public static Permission getPermissionProvider(final ServicesManager servicesManager) {
+        if (permission != null) {
+            return permission;
+        }
+
+        return (permission = BukkitProviderUtils.getProvider(servicesManager, Permission.class));
     }
 }
