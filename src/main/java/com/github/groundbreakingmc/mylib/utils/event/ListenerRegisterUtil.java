ea5d333d2300082e0473d7a48e7e9f6c6f00e81b
noslowdwn <noslowdwn@gmail.com>
2025-02-12 18:42:42 +0100

    OverwriteMC is monkeeeeeeeeeeyyyyyyyyyy

--- src/main/java/com/github/groundbreakingmc/mylib/utils/event/ListenerRegisterUtil.java
+++ src/main/java/com/github/groundbreakingmc/mylib/utils/event/ListenerRegisterUtil.java
@@ -1,5 +1,6 @@
 package com.github.groundbreakingmc.mylib.utils.event;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.UtilityClass;
 import org.bukkit.event.Event;
@@ -8,25 +9,26 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.EventExecutor;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
-@UtilityClass
+@UtilityClass @SuppressWarnings("unused")
 public final class ListenerRegisterUtil {
 
-    private static final Map<Listener, ListenerData> REGISTERED = new HashMap<>();
+    private static final Map<Listener, Set<ListenerData>> REGISTERED = new HashMap<>();
 
-    public static void register(final Plugin plugin,
-                                final Listener listener,
-                                final Class<? extends Event> eventClass,
-                                final EventPriority eventPriority,
-                                final boolean ignoreCancelled,
-                                final EventExecutor eventExecutor) {
-        final ListenerData listenerData = REGISTERED.get(listener);
-        if (listenerData != null && listenerData.equals(eventClass, eventPriority, eventExecutor)) {
-            throw new UnsupportedOperationException("Cannot register registered listener!");
+    public static boolean register(final Plugin plugin,
+                                   final Listener listener,
+                                   final Class<? extends Event> eventClass,
+                                   final EventPriority eventPriority,
+                                   final boolean ignoreCancelled,
+                                   final EventExecutor eventExecutor) {
+        final ListenerData listenerData = new ListenerData(eventClass, eventPriority, ignoreCancelled, eventExecutor);
+        final Set<ListenerData> listenerDataSet = REGISTERED.computeIfAbsent(listener, k -> new HashSet<>());
+
+        if (!listenerDataSet.isEmpty() && listenerDataSet.contains(listenerData)) {
+            return false;
         }
 
         plugin.getServer().getPluginManager().registerEvent(
@@ -37,17 +39,25 @@ public final class ListenerRegisterUtil {
                 plugin,
                 ignoreCancelled
         );
+        listenerDataSet.add(listenerData);
+        return true;
     }
 
-    public static void unregister(final Listener listener) {
-        if (!REGISTERED.containsKey(listener)) {
-            throw new UnsupportedOperationException("Cannot unregister not registered listener!");
+    public static boolean unregister(final Listener listener) {
+        if (REGISTERED.remove(listener) != null) {
+            return false;
         }
 
         HandlerList.unregisterAll(listener);
+        return true;
     }
 
-    public EventPriority getEventPriority(final String priority) {
+    @Nullable
+    public static EventPriority getEventPriority(final String priority) {
+        return getEventPriority(priority, null);
+    }
+
+    public static EventPriority getEventPriority(final String priority, final EventPriority defaultPriority) {
         switch (priority) {
             case "LOWEST":
                 return EventPriority.LOWEST;
@@ -60,16 +70,19 @@ public final class ListenerRegisterUtil {
             case "HIGHEST":
                 return EventPriority.HIGHEST;
             default:
-                throw new UnsupportedOperationException("Invalid event priority\"" + priority + "\"!");
+                return defaultPriority;
         }
     }
 
-    @RequiredArgsConstructor
-    private final class ListenerData {
-        public final Class<? extends Event> left;
-        public final EventPriority middle;
-        public final EventExecutor right;
+    @RequiredArgsConstructor @Getter
+    private static final class ListenerData {
 
+        private final Class<? extends Event> eventClass;
+        private final EventPriority priority;
+        private final boolean ignoreCancelled;
+        private final EventExecutor executor;
+
+        @Override
         public boolean equals(final Object object) {
             if (object == this) {
                 return true;
@@ -79,17 +92,29 @@ public final class ListenerRegisterUtil {
             }
 
             final ListenerData other = (ListenerData) object;
-            return Objects.equals(this.left, other.left)
-                    && Objects.equals(this.middle, other.middle)
-                    && Objects.equals(this.right, other.right);
+            return this.eventClass.isAssignableFrom(other.eventClass)
+                    && this.priority == other.priority
+                    && this.ignoreCancelled == other.ignoreCancelled
+                    && this.executor == other.executor;
         }
 
-        public boolean equals(final Object objectLeft,
-                              final Object middleRight,
-                              final Object objectRight) {
-            return Objects.equals(this.left, objectLeft)
-                    && Objects.equals(this.middle, middleRight)
-                    && Objects.equals(this.right, objectRight);
+        public boolean equals(final Class<? extends Event> eventClass,
+                              final EventPriority priority,
+                              boolean ignoreCancelled,
+                              final EventExecutor executor) {
+            return this.eventClass.isAssignableFrom(eventClass)
+                    && this.priority == priority
+                    && this.ignoreCancelled == ignoreCancelled
+                    && this.executor == executor;
+        }
+
+        public int hashCode() {
+            int result = 17;
+            result = (result << 5) + Objects.hashCode(this.eventClass);
+            result = (result << 5) * (this.priority != null ? priority.ordinal() : 0);
+            result = (result << 5) + (this.ignoreCancelled ? 1 : 0);
+            result = (result << 5) + Objects.hashCode(this.executor);
+            return result;
         }
     }
 }
