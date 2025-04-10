dadb3492a00beab8c15d89cb6282a620ba499cf2
GroundbreakingMC <noslowdwn@gmail.com>
2025-04-10 19:26:29 +0200

    Removed useless argument in method

--- src/main/java/com/github/groundbreakingmc/mylib/utils/server/ServerInfo.java
+++ src/main/java/com/github/groundbreakingmc/mylib/utils/server/ServerInfo.java
@@ -2,25 +2,25 @@ package com.github.groundbreakingmc.mylib.utils.server;
 
 import com.github.groundbreakingmc.mylib.logger.console.Logger;
 import lombok.experimental.UtilityClass;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.Bukkit;
 
 @UtilityClass
 public final class ServerInfo {
 
-    public int getSubVersion(final Plugin plugin, final Logger logger) {
+    public int getSubVersion() {
         try {
-            return Integer.parseInt(plugin.getServer().getMinecraftVersion().split("\\.", 3)[1]);
+            return Integer.parseInt(Bukkit.getServer().getMinecraftVersion().split("\\.", 3)[1]);
         } catch (final NumberFormatException ex) {
-            logger.warn("\u001b[32mFailed to extract server version. Plugin may not work correctly!");
+            Bukkit.getLogger().warning("\u001b[32mFailed to extract server version. Plugin may not work correctly!");
             return 0;
         }
     }
 
-    public int getSubVersion(final Plugin plugin) {
+    public int getSubVersion(final Logger logger) {
         try {
-            return Integer.parseInt(plugin.getServer().getMinecraftVersion().split("\\.", 3)[1]);
+            return Integer.parseInt(Bukkit.getServer().getMinecraftVersion().split("\\.", 3)[1]);
         } catch (final NumberFormatException ex) {
-            plugin.getLogger().warning("\u001b[32mFailed to extract server version. Plugin may not work correctly!");
+            logger.warn("\u001b[32mFailed to extract server version. Plugin may not work correctly!");
             return 0;
         }
     }
