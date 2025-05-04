b15e76707f7fcd82c30e362778a5ee0f426a1c06
GroundbreakingMC <noslowdwn@gmail.com>
2025-05-04 15:57:26 +0200

    Update ConfigUtils

--- src/main/java/com/github/groundbreakingmc/mylib/config/ConfigUtils.java
+++ src/main/java/com/github/groundbreakingmc/mylib/config/ConfigUtils.java
@@ -1,10 +1,12 @@
 package com.github.groundbreakingmc.mylib.config;
 
 import com.github.groundbreakingmc.mylib.colorizer.Colorizer;
+import com.github.groundbreakingmc.mylib.colorizer.legacy.StringColorizer;
 import com.github.groundbreakingmc.mylib.utils.player.settings.EffectSettings;
 import com.github.groundbreakingmc.mylib.utils.player.settings.SoundSettings;
 import com.github.groundbreakingmc.mylib.utils.player.settings.TitleSettings;
 import com.google.common.collect.ImmutableList;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -21,38 +23,41 @@ import java.util.List;
 @UtilityClass
 public class ConfigUtils {
 
-    public static List<String> getStringList(final ConfigurationNode node, final String path) throws SerializationException {
+    public static List<String> getStringList(ConfigurationNode node, String path) throws SerializationException {
         return node.node(path).getList(String.class);
     }
 
-    public static String getColorizedString(final ConfigurationNode node, final String path, final Colorizer colorizer) {
+    public static <T> T getColorizedString(ConfigurationNode node, String path, Colorizer<T> colorizer) {
         return getColorizedString(node, path, null, colorizer);
     }
 
-    public static String getColorizedString(final ConfigurationNode node, final String path, final String defaultValue, final Colorizer colorizer) {
+    public static <T> T getColorizedString(ConfigurationNode node, String path, String defaultValue, Colorizer<T> colorizer) {
         final String string = node.node(path).getString();
         return colorizer.colorize(string == null ? defaultValue : string);
     }
 
-    public static List<String> getColorizedStringList(final ConfigurationNode node, final String path, final Colorizer colorizer) throws SerializationException {
+    public static <T> List<T> getColorizedStringList(ConfigurationNode node, String path, Colorizer<T> colorizer) throws SerializationException {
         final List<String> list = getStringList(node, path);
-        list.replaceAll(colorizer::colorize);
-        return list;
+        final List<T> adapted = new ObjectArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            adapted.add(colorizer.colorize(list.get(i)));
+        }
+        return adapted;
     }
 
-    public static ImmutableList<String> getImmutableColorizedStringList(final ConfigurationNode node, final String path, final Colorizer colorizer) throws SerializationException {
+    public static ImmutableList<String> getImmutableColorizedStringList(ConfigurationNode node, String path, StringColorizer colorizer) throws SerializationException {
         return ImmutableList.copyOf(getColorizedStringList(node, path, colorizer));
     }
 
-    public static ImmutableList<String> getImmutableStringList(final ConfigurationNode node, final String path) throws SerializationException {
+    public static ImmutableList<String> getImmutableStringList(ConfigurationNode node, String path) throws SerializationException {
         return ImmutableList.copyOf(getStringList(node, path));
     }
 
-    public static ImmutableList<Material> getImmutableMaterialList(final ConfigurationNode node, final String path) throws SerializationException {
+    public static ImmutableList<Material> getImmutableMaterialList(ConfigurationNode node, String path) throws SerializationException {
         return ImmutableList.copyOf(getMaterialList(node, path));
     }
 
-    public static List<Material> getMaterialList(final ConfigurationNode node, final String path) throws SerializationException {
+    public static List<Material> getMaterialList(ConfigurationNode node, String path) throws SerializationException {
         final List<Material> materials = new ArrayList<>();
         final List<String> list = getStringList(node, path);
         if (list.isEmpty()) {
@@ -66,84 +71,72 @@ public class ConfigUtils {
         return materials;
     }
 
-    public static ImmutableList<World> getImmutableWorldList(final ConfigurationNode node, final String path) throws SerializationException {
+    public static ImmutableList<World> getImmutableWorldList(ConfigurationNode node, String path) throws SerializationException {
         return ImmutableList.copyOf(getWorldList(node, path));
     }
 
-    public static List<World> getWorldList(final ConfigurationNode node, final String path) throws SerializationException {
-        final List<World> worlds = new ArrayList<>();
+    public static List<World> getWorldList(ConfigurationNode node, String path) throws SerializationException {
         final List<String> list = getStringList(node, path);
+        final List<World> worlds = new ObjectArrayList<>(list.size());
         if (list.isEmpty()) {
             return worlds;
         }
 
-        for (final String worldName : list) {
-            worlds.add(Bukkit.getWorld(worldName));
+        for (int i = 0; i < list.size(); i++) {
+            worlds.add(Bukkit.getWorld(list.get(i)));
         }
 
         return worlds;
     }
 
-    public static ImmutableList<PotionEffectType> getImmutableEffectList(final ConfigurationNode node, final String path) throws SerializationException {
+    public static ImmutableList<PotionEffectType> getImmutableEffectList(ConfigurationNode node, String path) throws SerializationException {
         return ImmutableList.copyOf(getEffectList(node, path));
     }
 
-    public static List<PotionEffectType> getEffectList(final ConfigurationNode node, final String path) throws SerializationException {
-        final List<PotionEffectType> effectTypes = new ArrayList<>();
+    public static List<PotionEffectType> getEffectList(ConfigurationNode node, String path) throws SerializationException {
         final List<String> list = getStringList(node, path);
+        final List<PotionEffectType> effectTypes = new ObjectArrayList<>(list.size());
         if (list.isEmpty()) {
             return effectTypes;
         }
 
-        for (final String effectName : list) {
-            effectTypes.add(PotionEffectType.getByName(effectName));
+        for (int i = 0; i < list.size(); i++) {
+            effectTypes.add(PotionEffectType.getByName(list.get(i)));
         }
 
         return effectTypes;
     }
 
     @Nullable
-    public static EffectSettings getEffectSettings(final ConfigurationNode node, final String path) {
+    public static EffectSettings getEffectSettings(ConfigurationNode node, String path) {
         return getEffectSettings(node, path, null);
     }
 
     @Nullable
-    public static EffectSettings getEffectSettings(final ConfigurationNode node, final String path, final EffectSettings defaultValue) {
+    public static EffectSettings getEffectSettings(ConfigurationNode node, String path, EffectSettings defaultValue) {
         final String string = node.node(path).getString();
-        if (string == null) {
-            return defaultValue;
-        }
-
         return EffectSettings.fromString(string);
     }
 
     @Nullable
-    public static SoundSettings getSoundSettings(final ConfigurationNode node, final String path) {
+    public static SoundSettings getSoundSettings(ConfigurationNode node, String path) {
         return getSoundSettings(node, path, null);
     }
 
     @Nullable
-    public static SoundSettings getSoundSettings(final ConfigurationNode node, final String path, final SoundSettings defaultValue) {
+    public static SoundSettings getSoundSettings(ConfigurationNode node, String path, SoundSettings defaultValue) {
         final String string = node.node(path).getString();
-        if (string == null) {
-            return defaultValue;
-        }
-
         return SoundSettings.fromString(string);
     }
 
     @Nullable
-    public static TitleSettings getTitleSettings(final ConfigurationNode node, final String path) {
+    public static TitleSettings getTitleSettings(ConfigurationNode node, String path) {
         return getTitleSettings(node, path, null);
     }
 
     @Nullable
-    public static TitleSettings getTitleSettings(final ConfigurationNode node, final String path, final TitleSettings defaultValue) {
+    public static TitleSettings getTitleSettings(ConfigurationNode node, String path, TitleSettings defaultValue) {
         final String string = node.node(path).getString();
-        if (string == null) {
-            return defaultValue;
-        }
-
         return TitleSettings.fromString(string);
     }
 }
