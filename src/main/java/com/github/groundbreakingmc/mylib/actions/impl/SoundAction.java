5f8282928d6bf95510804efe9fd16e83d7d906c7
GroundbreakingMC <noslowdwn@gmail.com>
2025-12-13 16:28:54 +0100

    fix(sound): replace SoundSettings with Adventure Sound API
    SoundAction now uses net.kyori.adventure.sound.Sound instead of the custom SoundSettings.
    This should improve sound sending performance due to internal Adventure optimizations.

--- src/main/java/com/github/groundbreakingmc/mylib/actions/impl/SoundAction.java
+++ src/main/java/com/github/groundbreakingmc/mylib/actions/impl/SoundAction.java
@@ -2,25 +2,35 @@ package com.github.groundbreakingmc.mylib.actions.impl;
 
 import com.github.groundbreakingmc.mylib.actions.Action;
 import com.github.groundbreakingmc.mylib.actions.context.ActionContext;
-import com.github.groundbreakingmc.mylib.utils.player.PlayerUtils;
-import com.github.groundbreakingmc.mylib.utils.player.settings.SoundSettings;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import org.bukkit.entity.Player;
+import org.intellij.lang.annotations.Subst;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public final class SoundAction<C extends ActionContext> implements Action<C> {
 
-    private final SoundSettings soundSettings;
+    private final Sound sound;
 
-    public SoundAction(@NotNull String action) {
-        this.soundSettings = SoundSettings.fromString(action);
+    public SoundAction(@NotNull String raw) {
+        final String[] params = raw.split(";");
+        if (params.length == 0 || params[0].isEmpty()) {
+            throw new IllegalArgumentException("Sound name is missing");
+        }
+        @Subst("minecraft:entity.experience_orb.pickup") final String name = params[0].toLowerCase();
+        final float volume = params.length > 1 ? Float.parseFloat(params[1]) : 1f;
+        final float pitch = params.length > 2 ? Float.parseFloat(params[2]) : 1f;
+        final Sound.Source source = params.length > 3 ? Sound.Source.valueOf(params[3]) : Sound.Source.MASTER;
+
+        this.sound = Sound.sound(Key.key(name), source, volume, pitch);
     }
 
     @Override
     public void execute(@NotNull C context) {
         final Player player = context.getPlayer();
         if (player != null) {
-            PlayerUtils.playSound(player, this.soundSettings);
+            player.playSound(this.sound);
         }
     }
 }
