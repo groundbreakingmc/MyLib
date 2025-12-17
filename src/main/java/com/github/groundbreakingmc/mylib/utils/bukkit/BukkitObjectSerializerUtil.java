b8ab447fead9819641d48f4b622c04d854ea5c6e
MGG <mdbus@icloud.com>
2025-12-18 01:00:36 +0300

    fix(serializer): close streams with try-with-resources

--- src/main/java/com/github/groundbreakingmc/mylib/utils/bukkit/BukkitObjectSerializerUtil.java
+++ src/main/java/com/github/groundbreakingmc/mylib/utils/bukkit/BukkitObjectSerializerUtil.java
@@ -15,20 +15,19 @@ import java.io.ObjectOutputStream;
 public class BukkitObjectSerializerUtil {
 
     public static <T> byte @NotNull [] serialize(@NotNull T object) {
-        final ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-        try (final ObjectOutputStream outputStream = new BukkitObjectOutputStream(byteOutputStream)) {
+        try (final ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+             final ObjectOutputStream outputStream = new BukkitObjectOutputStream(byteOutputStream)) {
             outputStream.writeObject(object);
+            return byteOutputStream.toByteArray();
         } catch (final IOException ex) {
             throw new RuntimeException(ex);
         }
-
-        return byteOutputStream.toByteArray();
     }
 
     @SuppressWarnings("unchecked")
     public static <T> T deserialize(byte @NotNull [] blob, Class<T> clazz) {
-        try (final ByteArrayInputStream byteIn = new ByteArrayInputStream(blob)) {
-            final BukkitObjectInputStream in = new BukkitObjectInputStream(byteIn);
+        try (final ByteArrayInputStream byteIn = new ByteArrayInputStream(blob);
+             final BukkitObjectInputStream in = new BukkitObjectInputStream(byteIn)) {
             return (T) in.readObject();
         } catch (final IOException | ClassNotFoundException ex) {
             throw new RuntimeException(ex);
