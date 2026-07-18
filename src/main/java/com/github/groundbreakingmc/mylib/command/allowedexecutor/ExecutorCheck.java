33c7ca05d69d0350408a7305726d9e7c7cb4e809
GroundbreakingMC <noslowdwn@gmail.com>
2026-07-18 05:09:26 +0200

    refactor(commands)!: remove unused legacy annotation command system

--- src/main/java/com/github/groundbreakingmc/mylib/command/allowedexecutor/ExecutorCheck.java
+++
@@ -1,9 +0,0 @@
-package com.github.groundbreakingmc.mylib.command.allowedexecutor;
-
-import org.bukkit.command.CommandSender;
-
-public interface ExecutorCheck {
-    default boolean check(CommandSender sender) {
-        return true;
-    }
-}
